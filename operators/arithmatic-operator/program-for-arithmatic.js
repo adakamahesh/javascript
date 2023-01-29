e3bb691abedc9bc4b68255380d8c4ae3a154df53
adakamahesh <adakamahesh@gmail.com>
2023-01-29 19:14:35 +0530

    add arithmatic operator

---
+++ operators/arithmatic-operator/program-for-arithmatic.js
@@ -0,0 +1,8 @@
+let a=2;//it is used to intialization
+let b=4;//it is used to intialization
+console.log(a+b)//it is used to display add output
+console.log(a-b)//it is used to display sub output
+console.log(a*b)//it is used to display mul output
+console.log(a/b)//it is used to display div output
+console.log(a%b)//it is used to display mod output
+console.log(-(a))//it is used to display negate output
