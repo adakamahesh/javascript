de0f0d5970f9f851d9951b6bfe6baa3b480bf3bd
adakamahesh <adakamahesh@gmail.com>
2023-01-29 19:12:36 +0530

    add data types

---
+++ data-types/composite-data-type/identifie-composite-data-type.js
@@ -0,0 +1,8 @@
+let names=['mahesh','vasu','naveen','siva']//it is used to intialization
+console.log(names)//it is used to display the output
+console.log(typeof names)//it is used to identifie and display the output
+var students=[
+{name:'mahesh',roolnum:'202w1a0501',branch:'cse'},
+{name:'naveen',rollnum:'202w1a0549',branch:'cse'}]
+console.log(students)
+console.log (typeof students)
---
+++ data-types/primitive-data-type/identifie-primitive-data-type.js
@@ -0,0 +1,12 @@
+var x=9//it is used to intialization 
+console.log(x)//it is used to display the output
+console.log(typeof x)//it is used to identifie and display the data type 
+let y=8.9//it is used to intialization
+console.log(y)
+console.log(typeof y)
+let name="mahesh"
+console.log(name)
+console.log(typeof name)
+let sta=true
+console.log(sta)
+console.log(typeof sta)
