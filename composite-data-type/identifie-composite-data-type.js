5bbef1a5569f5d9d6be480d03b6b1e8431c09cd1
adakamahesh <adakamahesh@gmail.com>
2023-01-29 19:10:28 +0530

    delete the files

--- composite-data-type/identifie-composite-data-type.js
+++
@@ -1,8 +0,0 @@
-let names=['mahesh','vasu','naveen','siva']//it is used to intialization
-console.log(names)//it is used to display the output
-console.log(typeof names)//it is used to identifie and display the output
-var students=[
-{name:'mahesh',roolnum:'202w1a0501',branch:'cse'},
-{name:'naveen',rollnum:'202w1a0549',branch:'cse'}]
-console.log(students)
-console.log (typeof students)
--- specal-data-type/identifie-specal-data-type.js
+++
@@ -1,6 +0,0 @@
-let x//it is used to intialization
-console.log(x)//it is used to display the output
-console.log(typeof x)//it is used to identifie and display the output
-let y=null
-console.log(y)
-console.log(typeof y)
