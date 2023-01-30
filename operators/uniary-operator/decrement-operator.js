49fe775b1587af5e1090c80e404a532fbf16fbcc
adakamahesh <adakamahesh@gmail.com>
2023-01-30 21:18:05 +0530

    add the uniary operator program

---
+++ operators/uniary-operator/decrement-operator.js
@@ -0,0 +1,6 @@
+let a=4//it is used to intialization
+let b=5//it is used to intialization
+console.log(a--)//it is used to display post decrement operator
+console.log(a)//it is used to display a value after decrement operator 
+console.log(--b)//it is used to display the pre decrement operator
+console.log((--b)*(a--))//it is used to display the decrement operators 
---
+++ operators/uniary-operator/increment-operator.js
@@ -0,0 +1,6 @@
+let a=5//it is used to intialization
+let b=6//it is used to intialization
+console.log(a++)//it is used to display the post increment operator
+console.log(a)//it is used to display the a value after increment operator
+console.log(++b)//it is used to display the pre increment operator
+console.log((++a)+(b++))//it is used to display the output of increment operator
