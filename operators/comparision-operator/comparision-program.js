809a544f570a03b933da119915d000f530c70b08
adakamahesh <adakamahesh@gmail.com>
2023-02-01 21:38:13 +0530

    add comparision operator

---
+++ operators/comparision-operator/comparision-program.js
@@ -0,0 +1,10 @@
+let a=5//it is used to intialization
+let b=6//it is used to intialization
+let c=5//it is used to intilization
+console.log(a==c)//it is used to compar both values are equla are not if equal display true otherwise display false
+console.log(a!=b)//it is used to compar both values are not equal are not if not equal display true otherwise display false 
+console.log(a==b)//it is used to compar both values are equal are not if equal display true otherwise display false
+console.log(a>b)//it is used to compar both values 'a' is greater than to 'b' if greater than display true opherwise display false
+console.log(a>=c)//it is used to compar both values 'a'is greater than are equal to 'b' if greater than are equal display true otherwise false
+console.log(a<b)//it is used to compar both values 'a' is less than to 'b' if less than display true otherwise false
+console.log(a<=c)//it is used to compar both values 'a' is less tha are equal to 'c' if less than are equal display true otherwise false
