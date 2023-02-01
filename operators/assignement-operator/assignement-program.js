4bc869e623272642ac6ddeedf93a80ef67264fb9
adakamahesh <adakamahesh@gmail.com>
2023-02-01 21:35:51 +0530

    add to assignement operator

---
+++ operators/assignement-operator/assignement-program.js
@@ -0,0 +1,8 @@
+let a=6//it is used to intialization 
+let b=5//it is used to intialization
+console.log(a=b)//it is used to display 'b' value is assigned to 'a' 
+console.log(a+=b)//it is used to display 'a=a+b' value  
+console.log(a-=b)//it is used to display 'a=a-b' value
+console.log(a*=b)//it is used to display 'a=a*b' value
+console.log(a/=b)//it is used to display 'a=a/b' value
+console.log(a%=b)//it is used to display 'a=a%b' value
