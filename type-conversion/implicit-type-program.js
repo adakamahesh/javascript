0e1ea7ad1b995bda5f7d4477b757862d7786c931
adakamahesh <adakamahesh@gmail.com>
2023-02-02 22:16:09 +0530

    add implicit type conversion

---
+++ type-conversion/implicit-type-program.js
@@ -0,0 +1,11 @@
+console.log(3+"6")//it is used to display output is add of string and num 
+console.log(6+'m')//it is used to display output is add of string and num 
+console.log(6-'m')//it is used to display output is not a number becose 6-'m' is working is not possible 
+console.log(5+true)//it is used to display output is add 5+true wher true is consider the value is 1
+console.log(5+false)//it is used to display output is add 5+false wher false is consider the value is 0
+console.log(5/'2')//it is used to display output is div of string and num
+console.log(5/'m')//it is used to display output is not a number
+console.log(5*'2')//it is used to display output is mul of string and num
+console.log(5*'a')//it is used to display output is not a number
+console.log(5%"2")//it is used to display output is modl of string and num
+console.log(5%'b')//it is used to display output is not a number 
