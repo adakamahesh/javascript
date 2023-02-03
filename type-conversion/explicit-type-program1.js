cf1185887dad8f0e8795875b705d6b1aefeb016d
adakamahesh <adakamahesh@gmail.com>
2023-02-03 22:27:00 +0530

    add explicit type conversion

---
+++ type-conversion/explicit-type-program1.js
@@ -0,0 +1,18 @@
+var b="mahesh"//it is used to intialization 
+let x=Number(b)//it is used to intialization of type conversion part
+console.log(x)//it is used to display output of type conversion of intialization part
+console.log(typeof x)//it is used to display output is type of type conversion part
+let a="6"//it is used to intialization
+let y=Number(a)//it is used to intialization of type conversion
+console.log(y)//it is used to display output of type conversion part
+console.log(typeof y)//it is used to display output is type of type conversion part
+let c="34mb"//it is used to intialization 
+let z=parseInt(c)//it is used to intialization of type convesion
+console.log(z)//it is used to display output of type conversion part
+console.log(typeof z)//it is used to display output is type of type conversion part
+let d="6.5mb"//it is used to intialization
+let p=parseFloat(d)//it is used to intialization of type conversion
+console.log(p)//it is used to display output of type conversion part
+console.log(typeof p)//it is used to display output is type of type conversion part
+/*the above program we can't possible to convert of string alphabet in 
+type conversion*/
---
+++ type-conversion/explicit-type-program2.js
@@ -0,0 +1,12 @@
+let n=6//it is used to intialization
+let a=String(n)//it is used to intialization of type conversion
+console.log(a)//it is used to display output of intialization part
+console.log(typeof a)//it is used to display output is type of type conversion
+let x=5//it is used to intialization
+let b=Boolean(x)//it is used to intialization of type conversion
+console.log(b)//it is used to display output of intialization part
+console.log(typeof b)//it is used to display output is type of type conversion
+let y=0//it is used to intialization
+let c=Boolean(y)//it is used to intialization of type conversion
+console.log(c)//it is used to display output of intialization part
+console.log(typeof c)//it is used to display output is type of type conversion 
