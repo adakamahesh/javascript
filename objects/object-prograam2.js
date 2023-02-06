e5d663d1c0d5fae55f53f3b579baccef58a066bd
adakamahesh <adakamahesh@gmail.com>
2023-02-06 21:04:48 +0530

    add objects

---
+++ objects/object-prograam2.js
@@ -0,0 +1,13 @@
+let person={
+    name:"mahesh",
+    age:"20",
+    rullnum:"202w1a0501",
+    collage:"gvr&s"
+}//it is used to object creation
+function fullname()//it is function creation
+{
+    console.log("mahesh babu")
+}
+fullname()//it is used to function calling
+console.log(person)//it is used to display output
+console.log(person.name)//it is used to display output
---
+++ objects/object-program.js
@@ -0,0 +1,9 @@
+let person={
+    name:"mahesh",
+    age:"20",
+    studey:"b-tec"
+}//it is object creation
+console.log(person)//it is used to display output
+console.log(person.name)//it is used to display output
+console.log(person.studey)//it is used to display output
+console.log(person.age)//it is used to display output
