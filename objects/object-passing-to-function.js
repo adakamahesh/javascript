a4098ad6f7384f164d1b82149658f8be13e3bb1b
adakamahesh <adakamahesh@gmail.com>
2023-02-08 19:42:26 +0530

    add object passing function

---
+++ objects/object-passing-to-function.js
@@ -0,0 +1,12 @@
+function user(n,a,p)
+{
+    this.name=n
+    this.age=a
+    this.place=p
+}//it is used function creation
+user1=new user("mahesh babu",20,"ongole")//it  is user1 detailes
+user2=new user("vasu",21,"ongole")//it is user2 detailes
+user3=new user("siva",20,"gunter")//it is user3 detailes
+console.log(user1)//it is used to display output
+console.log(user3)//it is used to display output
+console.log(user2)//it is used to display output
---
+++ objects/update-and-add-data-in-object.js
@@ -0,0 +1,8 @@
+let user={
+    name:"mahesh",
+    age:"20",
+    place:"ongole"
+} //it is used to creat object
+user.name="mahesh babu"//it is used to modefiy the name
+user.class="3rd btech"//it is used to add the new data
+console.log(user)//it is used to display output 
