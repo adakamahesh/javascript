11f4835f5cf41b672a7ddcfbacc2f8e277b88136
adakamahesh <adakamahesh@gmail.com>
2023-02-09 19:58:27 +0530

    add arrays

---
+++ arrays/array-program1.js
@@ -0,0 +1,9 @@
+let names=['mahesh','jayy','mani','sai','aarush']//it is intialization of array
+console.log(names)//it is used to display output of array
+console.log(typeof names)//it is used to display output of type
+console.log(names.length)//it is used to display output is length of array
+console.log(Array.of('sri'))//it is used to create and display output of array
+console.log(names[0])//it is used to display output of name is mahesh
+console.log(names[4])//it is used to display output of name is aarush
+names[0]="mahesh babu"//it is used to modefie the array data
+console.log(names)//it is used to display output
